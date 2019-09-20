3128afd277512b85ac19690064dbc3409efe00c9
Mohammad Javed <ghazanfar9131@gmail.com>
2019-09-20 06:36:50 -0400

    P4 - Done

--- objects.js
+++ objects.js
@@ -44,3 +44,48 @@ delete monster.type;
 
 console.log(monster);
 
+
+/**************************/
+
+// P4
+
+const ogre = {
+    name:"Sejla",
+    hitpoints: 100,
+    attack: function () {
+       return Math.floor(Math.random()*10)
+    }
+};
+
+const adventurer = {
+    name:"Candy",
+    hitpoints: 100,
+    attack: function () {
+        return 10
+    }
+};
+
+let winner = null;
+
+for (let i =true;true;i=!i){
+    if(i){ // when i is true it's Ogre turns
+        const damage = ogre.attack();
+        console.log(`${ogre.name} attacks on ${adventurer.name} with ${damage} Damage.`);
+        adventurer.hitpoints-=damage;
+    }
+    else{
+        const damage = adventurer.attack();
+        console.log(`${adventurer.name} attacks on ${ogre.name} with ${damage} Damage.`);
+        ogre.hitpoints-=damage;
+    }
+    if(ogre.hitpoints <0){
+        winner = adventurer;
+        break;
+    }
+    if(adventurer.hitpoints <0){
+        winner = ogre;
+        break;
+    }
+}
+console.log(`${winner.name} Wins!!`);
+
